e8ff42549170969e8215bc3c0669d2ff3d8a78ad
nkunczer <n.kunczer@gmail.com>
2023-03-13 09:40:04 +0100

    footer.php nav and back button integrated

---
+++ src/footer.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+
+<footer id="footer">
+    <nav id="footer-nav">
+      <ul>
+        <li><a href="#">Datenschutz</a></li>
+        <li><a href="#">Impressum</a></li>
+      </ul>
+    </nav>
+  </footer>
+
+<a id="back-btn" href="#">  &#128285  </a>
+  
+
+
+</body>
+
+</html>
