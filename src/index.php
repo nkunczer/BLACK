4cfdeb625e09bffad5c95530c6790474dd898d63
nkunczer <n.kunczer@gmail.com>
2023-03-13 09:45:20 +0100

    index.php footer and header connected - content integrated

---
+++ src/index.php
@@ -0,0 +1,120 @@
+<?php
+?>
+
+<?php get_header(); ?>
+
+<main>
+
+
+
+
+
+
+<div class="background">
+        
+        </div>
+
+
+    <h1>"Black is Blyatiful"</h1>
+
+    <hr>
+    
+    <br>
+
+    <p>Schwarz ist eine Farbe, die seit jeher eine besondere Bedeutung und Symbolik besitzt. In vielen Kulturen und Epochen wurde Schwarz als Farbe der Eleganz, der Stärke und der Autorität angesehen. In der westlichen Kultur wird Schwarz oft mit Trauer und Tod in Verbindung gebracht, aber es hat auch positive Assoziationen wie Stil, Klasse und Macht.</p>
+
+
+    <h2>Die Natur von Schwarz</h2>
+    <p>Aus physikalischer Sicht ist Schwarz keine Farbe, sondern das Fehlen von Licht. Die Farben, die wir sehen, sind das Ergebnis der Reflexion von Licht von Oberflächen. Wenn Licht auf eine Oberfläche trifft, wird ein Teil des Lichts absorbiert und ein Teil wird reflektiert. Die reflektierte Farbe ist das, was wir sehen.
+
+      <br> <br>
+      Ein schwarzes Objekt absorbiert alle Farben des sichtbaren Spektrums, einschließlich Rot, Orange, Gelb, Grün, Blau und Violett. Es absorbiert auch Infrarot- und Ultraviolettstrahlung. Da kein Licht reflektiert wird, wird die Energie des Lichts in Wärme umgewandelt.
+      <br> <br>
+      Schwarz hat auch eine bedeutende Rolle in der Optik und in der Entwicklung von lichtempfindlichen Materialien gespielt. Es wurde entdeckt, dass eine Schicht aus Silberhalogenid auf schwarzem Papier das erste lichtempfindliche Material war. Dies wurde als Grundlage für die Entwicklung von Fotografie und Film verwendet.</p>
+
+      <br> <br>
+ 
+    
+
+      <table class="responsive-table">
+        <thead>
+          <tr>
+            <th>FARBE</th>
+            <th>RGB CODE</th>
+            <th class="name-column">NAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="background-color: #000000; border-radius: 5px;"></td>
+            <td>RGB(0, 0, 0)</td>
+            <td class="name-column">Schwarz</td>
+          </tr>
+          <tr>
+            <td style="background-color: #333333; border-radius: 5px;"></td>
+            <td>RGB(51, 51, 51)</td>
+            <td class="name-column">Dunkelgrau</td>
+          </tr>
+          <tr>
+            <td style="background-color: #666666; border-radius: 5px;"></td>
+            <td>RGB(102, 102, 102)</td>
+            <td class="name-column">Grau</td>
+          </tr>
+          <tr>
+            <td style="background-color: #999999; border-radius: 5px;"></td>
+            <td>RGB(153, 153, 153)</td>
+            <td class="name-column">Hellgrau</td>
+          </tr>
+          <tr>
+            <td style="background-color: #CCCCCC; border-radius: 5px;"></td>
+            <td>RGB(204, 204, 204)</td>
+            <td class="name-column">Grauweiß</td>
+          </tr>
+          <tr>
+            <td style="background-color: #FFFFFF; border-radius: 5px;"></td>
+            <td>RGB(255, 255, 255)</td>
+            <td class="name-column">Weiß</td>
+          </tr>
+        </tbody>
+      </table>
+      
+      <br> <br>
+    
+
+    <h2>4 geschichtliche Ansätze zur Farbe Schwarz</h2>
+    <div class="flex-container-2">
+        <div class="box box-2"><b>1. </b>Die Farbe Schwarz hat eine reiche Geschichte und symbolisiert oft Geheimnisvolles, Mysteriöses und Intellektuelles. Die antiken Griechen, zum Beispiel, sahen die Farbe Schwarz als die Farbe der Bildung und Weisheit. Die Philosophen trugen schwarze Gewänder, um ihre Gelehrsamkeit und ihren intellektuellen Status zu demonstrieren. Kleriker und Adelige trugen schwarze Kleidung, um ihre Würde und Autorität zu unterstreichen.</div>
+        <div class="box box-2"><b>2. </b>Schwarz kann auch eine symbolische Bedeutung haben. In vielen Kulturen wird es mit Trauer und Tod in Verbindung gebracht. Aber auch hier kann die Farbe eine positive Interpretation haben. Sie kann Trost spenden und die endgültige Erlösung symbolisieren.</div>
+        <div class="box box-2"><b>3. </b>Ein Grund, warum Schwarz als die beste aller Farben betrachtet werden kann, ist, dass es eine gewisse Eleganz und Raffinesse ausstrahlt. Schwarz kann einen einfachen Gegenstand in ein Kunstwerk verwandeln. Es gibt einen gewissen Hauch von Mystik und Intellektualität, der mit dieser Farbe verbunden ist. Schwarz kann auch eine starke emotionale Wirkung haben. </div>
+        <div class="box box-2"><b>4. </b>In der heutigen Gesellschaft hat Schwarz auch eine politische Bedeutung. Es kann Rebellion und Anti-Establishment symbolisieren. Es ist eine Farbe, die von vielen als Ausdruck des Protests verwendet wird. Schwarze Kleidung kann auch eine soziale Identität darstellen und eine starke Aussage machen.</div>
+    </div>
+
+
+
+
+    <br> <br>
+
+
+
+    <form id="form" action="mailto:empfaenger@domain.com" method="post"> 
+      <label for="nachricht">Are you a black lovin person?</label> 
+      <textarea id="nachricht" name="nachricht" rows="3" required>Type your answer here and push the button..</textarea> 
+      <br> 
+      <button type="submit">..the button</button> </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+
+<?php get_footer(); ?>
