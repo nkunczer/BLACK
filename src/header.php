635b891131f1c21e746564f6472d5c643c9ca9b0
nkunczer <n.kunczer@gmail.com>
2023-03-13 09:36:55 +0100

    header.php head meta info and nav integrated

---
+++ src/header.php
@@ -0,0 +1,50 @@
+<?php ?>
+<!DOCTYPE html>
+
+<?php ?>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <?php ?>
+    <title>BLACK IS BLYATIFUL</title>
+    <meta charset="<?php bloginfo('charset'); ?>>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Definitionen von Schwarz - einfach erklärt">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="author" content="Copyleft">
+<meta name="robots" content="noindex, nofollow">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="shortcut icon" href="pfad/zum/favico.ico" type="image/x-icon">
+<link rel="icon" href="img/fav.png" type="image/x-icon">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+
+
+    <?php wp_head(); 
+    ?>
+
+
+
+
+</head>
+<?php /* body_class() liefert viele nützliche Klassen-Namen aus WordPress. ZB: logged-in, admin-bar, template, post-id, etc.
+     * https://developer.wordpress.org/reference/functions/body_class/
+     */ ?>
+<body <?php body_class(); ?>>
+<nav>
+          <ul>
+            <li><a href="https://de.wikipedia.org/wiki/Farbenlehre_(Goethe)" target="_blank">Wikipedia</a>
+            
+              
+            
+            </li>
+            <li><a href="#">Admin</a></li>
+           
+           
+            
+          </ul>
+        </nav>
