14b579c95ec98b67c622e3df8b222e2098708749
nkunczer <n.kunczer82@gmail.com>
2023-03-20 15:20:48 +0100

    404 page styling added

--- BLACK/404.php
+++ BLACK/404.php
@@ -1,7 +1,77 @@
-<?php get_header( ); ?>
+
+
+<!DOCTYPE html>
+
+<?php ?>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <?php ?>
+    <title>BLACK IS BLYATIFUL</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Definitionen von Schwarz - einfach erklärt">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="author" content="Copyleft">
+    <meta name="robots" content="noindex, nofollow">
+
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <link rel="shortcut icon" href="assets/img/fav.ico" type="image/x-icon">
+    <link rel="icon" href="assets/img/fav.png" type="image/x-icon">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+
+
+<?php wp_head(); 
+?>
+
+
+
+
+<head>
+    <title>404 Error</title>
+    <style>
+      /* Definiert die blinkende Animation */
+      @keyframes blink {
+        50% {
+          opacity: 0;
+        }
+      }
+      
+      /* Definiert den Stil des Texts */
+      #error-text {
+        font-size: 40em;
+        text-align: center;
+        color: red;
+        animation: blink 1s linear infinite;
+      }
+      
+      /* Setzt den Flex-Container auf die gesamte Höhe und Breite des Viewports */
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+      }
+      
+      /* Zentriert den Text innerhalb des Flex-Containers */
+      #error-text {
+        text-align: center;
+      }
+    </style>
+  </head>
+
+
+
+
 <main>
 
-<h1>hallo</h1>
+
+
+<h1 id="error-text">404 ERROR</h1>
 
 </main>
-<?php get_footer( ) ; ?>
