ffd4b93cebca63a0d04d324a0b93a84467c8e8c2
nkunczer <n.kunczer82@gmail.com>
2023-03-20 14:03:57 +0100

    align-wide fixed functions.php

--- BLACK/functions.php
+++ BLACK/functions.php
@@ -1,5 +1,27 @@
 <?php
 
+
+add_action('after_setup_theme', function () {
+
+    
+    add_theme_support('title-tag');
+
+    add_theme_support('wp-block-styles');
+
+ 
+    add_theme_support('align-wide');
+
+ 
+    add_theme_support('editor-styles');
+
+    add_theme_support('responsive-embeds');
+
+    add_filter('show_admin_bar', '__return_false');
+
+});
+
+
+
 add_action('wp_enqueue_scripts', function () use ($version) {
     
     wp_enqueue_style('webdev-css', get_template_directory_uri() . '/style.css');
