2dffd3463453d1f9bbd3be5ae13d8b0a4e71999e
nkunczer <n.kunczer82@gmail.com>
2023-03-20 11:06:18 +0100

    style.css updated

--- BLACK/footer.php
+++ BLACK/footer.php
@@ -4,8 +4,8 @@
 <footer id="footer">
     <nav id="footer-nav">
       <ul>
-        <li><a href="https://webdev-02-22-02-project.wifiblogs.at/datenschutz/">Datenschutz</a></li>
-        <li><a href="https://webdev-02-22-02-project.wifiblogs.at/impressum/">Impressum</a></li>
+        <li><a href="#">Datenschutz</a></li>
+        <li><a href="#">Impressum</a></li>
       </ul>
     </nav>
   </footer>
